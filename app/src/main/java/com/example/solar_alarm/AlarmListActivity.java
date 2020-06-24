7bde12e7c44cdbcc702a72912643915d2ad9564a
Daniel Ortiz <DOrtiz1011@gmail.com>
2020-06-23 23:26:50 -0400

    Update AlarmListActivity.java

--- app/src/main/java/com/example/solar_alarm/AlarmListActivity.java
+++ app/src/main/java/com/example/solar_alarm/AlarmListActivity.java
@@ -3,15 +3,25 @@ package com.example.solar_alarm;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class AlarmListActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter alarmAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_list);
 
-        RecyclerView recyclerView = findViewById(R.id.recycleViewer);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView = findViewById(R.id.recycleViewer);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        alarmAdapter = new AlarmAdapter();   // add dataset to this new instance
+        recyclerView.setAdapter(alarmAdapter);
     }
 }
