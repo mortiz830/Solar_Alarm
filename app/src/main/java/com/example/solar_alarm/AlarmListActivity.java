a133d26bce21e4c6f44f6ee337d08c961ced949d
mortiz830 <ortim7@farmingdale.edu>
2020-08-17 19:01:58 -0400

    Added Fragment

--- app/src/main/java/com/example/solar_alarm/AlarmListActivity.java
+++ app/src/main/java/com/example/solar_alarm/AlarmListActivity.java
@@ -1,12 +1,21 @@
 package com.example.solar_alarm;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.time.LocalTime;
 
 /**
  * https://developer.android.com/guide/topics/ui/layout/recyclerview
@@ -26,8 +35,30 @@ public class AlarmListActivity extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
+        // FRAGMENTS
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        final AlarmSetFragment alarmSetFragment = new AlarmSetFragment();
+
+
+        final FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.add(R.id.flFragment, alarmSetFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
         // MOCKED DATA
-        alarmAdapter = new AlarmAdapter(Alarm.createAlarmList(10));
+        alarmAdapter = new AlarmAdapter(Alarm.createAlarmList());
         recyclerView.setAdapter(alarmAdapter);
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void addNewAlarm(String l1, String s1){
+
+    }
 }
+
