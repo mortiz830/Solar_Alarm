bafa5aa79eab6af7eee1d3a83fe0f7423afcaa4e
Daniel Ortiz <danielortiz@filevine.com>
2021-04-12 19:03:27 -0500

    test fules

---
+++ app/src/test/java/com/example/solar_alarm/CSVTestDriver.java
@@ -0,0 +1,47 @@
+package com.example.solar_alarm;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Simple class to read a CSV file and return a list of arrays of n strings that comprise a line in the CSV
+ * file.
+ */
+public class CSVTestDriver {
+
+    private File testDataDirectory;
+
+    public CSVTestDriver(String testDataDirectoryName) {
+        testDataDirectory = new File(testDataDirectoryName);
+    }
+
+    public String[] getFileNames() {
+        return testDataDirectory.list();
+    }
+
+    public List<String[]> getData(String testDataFileName) {
+        List<String[]> valueList = new ArrayList<String[]>();
+        try {
+            FileReader csvFileReader = new FileReader(new File(testDataDirectory + "/" + testDataFileName));
+            LineNumberReader reader = new LineNumberReader(csvFileReader);
+
+            String line;
+            try {
+                while ((line = reader.readLine()) != null) {
+                    String[] datum = line.split("\\,");
+                    valueList.add(datum);
+                }
+            } catch (FileNotFoundException fnfe) {
+                fnfe.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return valueList;
+    }
+}
---
+++ app/src/test/java/com/example/solar_alarm/SunriseSunsetCalculatorTest.java
@@ -0,0 +1,95 @@
+package com.example.solar_alarm;
+
+import com.example.solar_alarm.SolarCalculator.Location;
+import com.example.solar_alarm.SolarCalculator.SunriseSunsetCalculator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Calendar;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unit test for the SunriseSunsetCalculator class.
+ */
+public class SunriseSunsetCalculatorTest extends BaseTestCase {
+
+    private SunriseSunsetCalculator calc;
+
+    @Before
+    public void setup() {
+        // November 1, 2008
+        super.setup(10, 1, 2008);
+        calc = new SunriseSunsetCalculator(location, "America/New_York");
+    }
+
+    @Test
+    public void testComputeAstronomicalSunrise() {
+        assertTimeEquals("06:01", calc.getAstronomicalSunriseForDate(eventDate), eventDate.getTime().toString());
+    }
+
+    @Test
+    public void testComputeAstronomicalSunset() {
+        assertTimeEquals("19:32", calc.getAstronomicalSunsetForDate(eventDate), eventDate.getTime().toString());
+    }
+
+    @Test
+    public void testComputeNauticalSunrise() {
+        assertTimeEquals("06:33", calc.getNauticalSunriseForDate(eventDate), eventDate.getTime().toString());
+    }
+
+    @Test
+    public void testComputeNauticalSunset() {
+        assertTimeEquals("19:00", calc.getNauticalSunsetForDate(eventDate), eventDate.getTime().toString());
+    }
+
+    @Test
+    public void testComputeCivilSunrise() {
+        assertTimeEquals("07:05", calc.getCivilSunriseForDate(eventDate), eventDate.getTime().toString());
+    }
+
+    @Test
+    public void testComputeCivilSunset() {
+        assertTimeEquals("18:28", calc.getCivilSunsetForDate(eventDate), eventDate.getTime().toString());
+    }
+
+    @Test
+    public void testComputeOfficialSunrise() {
+        assertTimeEquals("07:33", calc.getOfficialSunriseForDate(eventDate), eventDate.getTime().toString());
+    }
+
+    @Test
+    public void testComputeOfficialSunset() {
+        assertTimeEquals("18:00", calc.getOfficialSunsetForDate(eventDate), eventDate.getTime().toString());
+    }
+
+    @Test
+    public void testSpecificDateLocationAndTimezone() {
+        Location loc = new Location("55.03", "82.91");
+        SunriseSunsetCalculator calculator = new SunriseSunsetCalculator(loc, "GMT");
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2012, 4, 7);
+
+        String officialSunriseForDate = calculator.getOfficialSunriseForDate(calendar);
+        assertEquals("22:35", officialSunriseForDate);
+
+        Calendar officialSunriseCalendarForDate = calculator.getOfficialSunriseCalendarForDate(calendar);
+        assertEquals(22, officialSunriseCalendarForDate.get(Calendar.HOUR_OF_DAY));
+        assertEquals(35, officialSunriseCalendarForDate.get(Calendar.MINUTE));
+        assertEquals(6, officialSunriseCalendarForDate.get(Calendar.DAY_OF_MONTH));
+    }
+
+    @Test
+    public void testNonIntegerTimezoneOffset() {
+        Location loc = new Location("22.56", "88.36");
+        SunriseSunsetCalculator calculator = new SunriseSunsetCalculator(loc, "Asia/Kolkata");
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2014, 12, 15);
+
+        String officialSunriseForDate = calculator.getOfficialSunriseForDate(calendar);
+        assertEquals("06:19", officialSunriseForDate);
+    }
+}
