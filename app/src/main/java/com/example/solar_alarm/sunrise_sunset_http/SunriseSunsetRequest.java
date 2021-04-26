500ff9964fbfbc75ebe51177565f6b1a2968a9af
mortiz830 <ortim7@farmingdale.edu>
2021-04-26 19:49:43 -0400

    SunriseSunsetRequest

--- app/src/main/java/com/example/solar_alarm/sunrise_sunset_http/SunriseSunsetRequest.java
+++ app/src/main/java/com/example/solar_alarm/sunrise_sunset_http/SunriseSunsetRequest.java
@@ -6,6 +6,54 @@ public class SunriseSunsetRequest
     public float Longitude;
     public String Date;
     public Boolean Format;
+
+    public SunriseSunsetRequest(float latitude, float longitude, String date, Boolean format)
+    {
+        this.Latitude = latitude;
+        this.Longitude = longitude;
+        this.Date = date;
+        this.Format = format;
+    }
+
+    public void setLatitude(float latitude)
+    {
+        this.Latitude = latitude;
+    }
+
+    public void setLongitude(float longitude)
+    {
+        this.Longitude = longitude;
+    }
+
+    public void setDate(String date)
+    {
+        this.Date = date;
+    }
+
+    public void setFormat(Boolean format)
+    {
+        this.Format = format;
+    }
+
+    public float getLatitude()
+    {
+        return Latitude;
+    }
+
+    public float getLongitude()
+    {
+        return Longitude;
+    }
+
+    public String getDate()
+    {
+        return Date;
+    }
+
+    public Boolean getFormat()
+    {
+        return Format;
+    }
 }
 
 
