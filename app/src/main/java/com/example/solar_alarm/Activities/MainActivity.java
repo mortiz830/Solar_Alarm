ef795698ec0b447514ea569f564a5e59b0c10b4b
Daniel Ortiz <DanielOrtiz@FileVine.com>
2021-05-25 18:51:55 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/solar_alarm/Activities/MainActivity.java
+++ app/src/main/java/com/example/solar_alarm/Activities/MainActivity.java
@@ -8,17 +8,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.solar_alarm.R;
 
 public class MainActivity extends AppCompatActivity {
-
-
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
-
-
 }
-
-
