c38746797c852ba09ca2bb1e988062b1680b300b
mortiz830 <ortim7@farmingdale.edu>
2021-05-25 19:25:31 -0400

    Delete unused in MainActivity

--- app/src/main/java/com/example/solar_alarm/Activities/MainActivity.java
+++ app/src/main/java/com/example/solar_alarm/Activities/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.solar_alarm.Activities;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.solar_alarm.R;
+
 public class MainActivity extends AppCompatActivity {
-    /*
-    FusedLocationProviderClient fusedLocationProviderClient;
+
 
 
     @Override
@@ -12,43 +16,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-        {
-            getLocation();
-
-        }
-        else {
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]
-                    {Manifest.permission.ACCESS_FINE_LOCATION}, 44);
-        }
-
-
     }
 
-    private void getLocation() {
-        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
 
-
-            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<android.location.Location>() {
-                @Override
-                public void onComplete(@NonNull Task<android.location.Location> task) {
-                    android.location.Location location = task.getResult();
-
-                    if (location != null) {
-                        Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
-                        try {
-                            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-                            mlocation.setLocation(addresses.get(0).getLatitude(), addresses.get(0).getLongitude());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                }
-            });
-        }
-    }*/
 }
 
 
