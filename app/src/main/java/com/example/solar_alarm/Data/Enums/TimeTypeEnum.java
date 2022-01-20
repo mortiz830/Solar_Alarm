e14d8580441da8aac7d687a74a5ddf8f7ef422a2
Daniel Ortiz <danielortiz@filevine.com>
2022-01-19 19:42:49 -0600

    Update TimeTypeEnum.java

--- app/src/main/java/com/example/solar_alarm/Data/Enums/TimeTypeEnum.java
+++ app/src/main/java/com/example/solar_alarm/Data/Enums/TimeTypeEnum.java
@@ -4,13 +4,13 @@ public enum TimeTypeEnum
 {
     Sunrise(1, "Sunrise"),
     Sunset(2, "Sunset"),
-    SolarNoon(3, "SolarNoon");//,
-    //CivilTwilightBegin(4),
-    //CivilTwilightEnd(5),
-    //NauticalTwilightBegin(6),
-    //NauticalTwilightEnd(7),
-    //AstronomicalTwilightBegin(8),
-    //AstronomicalTwilightEnd(9)
+    SolarNoon(3, "SolarNoon"),
+    CivilTwilightBegin(4, "Civil Twilight Begin"),
+    CivilTwilightEnd(5, "Civil Twilight End"),
+    NauticalTwilightBegin(6, "Nautical Twilight Begin"),
+    NauticalTwilightEnd(7, "Nautical Twilight End"),
+    AstronomicalTwilightBegin(8, "Astronomical Twilight Begin"),
+    AstronomicalTwilightEnd(9, "Astronomical Twilight End");
 
     public final int    Id;
     public final String Name;
