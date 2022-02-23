97a2c639f2fbae94dc2f63f5362d5b9e9da8e2b2
Daniel Ortiz <danielortiz@filevine.com>
2022-02-22 18:48:37 -0600

    Update StaticDataMigration.java

--- app/src/main/java/com/example/solar_alarm/Data/Migrations/StaticDataMigration.java
+++ app/src/main/java/com/example/solar_alarm/Data/Migrations/StaticDataMigration.java
@@ -1,9 +1,12 @@
 package com.example.solar_alarm.Data.Migrations;
 
+import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
+
 import android.os.Build;
 
 import androidx.annotation.RequiresApi;
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.migration.Migration;
 import androidx.sqlite.db.SupportSQLiteDatabase;
@@ -11,6 +14,7 @@ import androidx.sqlite.db.SupportSQLiteDatabase;
 import com.example.solar_alarm.Data.Enums.AlarmTypeEnum;
 import com.example.solar_alarm.Data.Enums.SolarTimeTypeEnum;
 import com.example.solar_alarm.Data.Enums.TimeUnitTypeEnum;
+import com.example.solar_alarm.Data.SolarAlarmDatabase;
 import com.example.solar_alarm.Data.Tables.AlarmType;
 import com.example.solar_alarm.Data.Tables.SolarTimeType;
 import com.example.solar_alarm.Data.Tables.TimeUnitType;
@@ -72,6 +76,8 @@ public abstract class StaticDataMigration extends RoomDatabase
             database.execSQL(sql);
 
             database.endTransaction();
+
+            Room.databaseBuilder(getApplicationContext(), SolarAlarmDatabase.class, "SolarAlarmDatabase").addMigrations(MIGRATION_1_2).build();
         }
 
         //Room.databaseBuilder(Context, SolarAlarmDatabase.class, "SolarAlarmDatabase").addMigrations(MIGRATION_1_2).build();
