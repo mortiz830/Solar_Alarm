1621ef9b02dbbbdfe916c3658632be7233f7567a
mortiz830 <mortiz830@gmail.com>
2025-01-07 19:09:41 -0500

    LocationItem and Location List layouts

---
+++ app/src/main/java/com/example/solar_alarm/Location/LocationAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.solar_alarm.Location
+
+import android.location.Location
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class LocationAdapter(private val locations: List<Location>) : RecyclerView.Adapter<LocationAdapter.LocationViewHolder>()
+{
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LocationAdapter.LocationViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: LocationAdapter.LocationViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
--- app/src/main/java/com/example/solar_alarm/Location/LocationViewHolder.kt
+++ app/src/main/java/com/example/solar_alarm/Location/LocationViewHolder.kt
@@ -2,8 +2,8 @@ package com.example.solar_alarm.Location
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import com.example.solar_alarm.AlarmList.OnToggleAlarmListener
 
-class LocationViewHolder (itemView: View, onToggleAlarmListener: OnToggleAlarmListener) : RecyclerView.ViewHolder(itemView)
+class LocationViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView)
 {
+    val
 }
